1c2525943e5e5d995df21d38e3b7db4f799f4f3b
Jadan Kerr <badi1863@gmail.com>
2023-09-18 17:42:44 -0600

    Wrote some things!

--- src/Main.java
+++ src/Main.java
@@ -9,19 +9,26 @@ public class Main {
 
     }
     public static void bottleCounter(int startNum) {
-        for(int i =startNum; (i < startNum + 1) & (i >= 0); i--){
-            if (i!=1) {
-                System.out.print(i + " bottles of beer on the wall, " + i + " bottles of beer.\n Take one down and pass it around, ");
 
-                if ((i-1)<=0) {
-                    System.out.println("Go to the store and buy some more, "+ startNum + " bottles of beer on the wall");
-                }else {
-                    System.out.println(i-1 + " bottles of beer on the wall.");
+        for(int i = startNum; i >= 0; i--){
+            //First conditional, if i is not 1, print with the plural text
+            if (i!=1) {
+                System.out.print(i + " bottles of beer on the wall, " + i + " bottles of beer.\n");
+                //If i is zero, print special "go to the store" string
+                if (i == 0) {
+                    System.out.print("Go to the store and buy some more, "+ startNum + " bottles of beer on the wall.\n\n");
                 }
+                //if i is 2, we know the next value will be one, and therefore will require singular grammar
+                else if (i == 2) {
+                    System.out.print("Take one down and pass it around, " + (i-1) + " bottle of beer on the wall.\n\n");
+                }
+                //If i is any other arbitrary number, print standard plural form
+                System.out.print("Take one down and pass it around, " + (i-1) + " bottles of beer on the wall.\n\n");
             }
             else {
-                System.out.println(i + " bottle of beer on the wall");
-
+                //If i is 1, then you just need to print the singular text form and the next "0 bottles of beer on the wall"
+                System.out.print("1 bottle of beer on the wall, 1 bottle of beer. \n");
+                System.out.println("Take one down and pass it around, 0 bottles of beer on the wall.\n");
             }
         }
 
